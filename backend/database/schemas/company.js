71aef211226337da30423e20bad64643b8cd3600
David Mas-Bagà <davidmasbaga@gmail.com>
2022-12-16 11:43:40 +0100

    Creación Schema Company v1

---
+++ backend/database/schemas/company.js
@@ -0,0 +1,17 @@
+const { Schema, model }  = require('mongoose');
+
+const companySchema = new Schema({
+    id: {type:Number},
+    name:  {type: String, required: true}, 
+    website: {type: String},
+    sector: {type: String},
+    company_logo_url: {type: String},
+    company_size: {type: String},
+    company_type: {type: String},
+    company_description: {type: String},
+    
+});
+
+const Company = model('company', companySchema);
+
+module.exports = Company;
