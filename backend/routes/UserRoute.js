cd5bcb3a8e5bd69d1b9a00755fcf03ebb8bb42d8
dadaisma <114525596+dadaisma@users.noreply.github.com>
2022-12-16 08:22:27 +0100

    db\sprint1\LIN-41\user_endpoints

---
+++ backend/routes/UserRoute.js
@@ -0,0 +1,15 @@
+const { Router } = require("express");
+
+const UserModel = require("../mongo/schema/user/user")
+
+ const {registerUser, getUserList, editUser, deleteUser } = require("../utils/UserController");
+ 
+ const routerUser = Router();
+
+
+routerUser.post("/user", registerUser);
+  routerUser.get("/user", getUserList);
+  routerUser.patch/("/user/:id", editUser);
+  routerUser.delete/("/user/:id", deleteUser);
+
+  module.export = routerUser
---
+++ backend/utils/UserController.js
@@ -0,0 +1,53 @@
+//necesitaremos un middleware  const {validateTodo, addDateMiddleware} = require('../middleware');
+const UserModel = require("../mongo/schema/user/user");
+
+const getUserList = async (req, res) => {
+    const user = await UserModel.find();
+    res.json(user);
+  }
+  
+  
+  const registerUser = async (req, res)=>{
+  
+    const body = req.body
+      const {username,password}  = body
+     
+      const data = {
+          username : username,
+          password : password
+         
+      }
+      const newUser = new UserModel(data)
+      await newUser.save()
+      res.json(newUser);
+  }
+  
+  const editUser = async (req, res) =>{
+    console.log(req)
+    const {id} = req.params
+    const user = await UserModel.findByIdAndUpdate(id, req.body);
+    res.json(user);
+  }
+  
+  const deleteUser = (req, res) => {
+    UserModel.findByIdAndDelete(req.params.id, (err, user ) =>{
+        if(err){
+            return res.status(500).json(err)
+        }
+        res.status(200).json(todo)
+    })
+}
+
+const findUser = async (req, res) => {
+    const {id} = req.params
+    const user = await UserModel.find(id, req.body);
+    res.json(user);
+  }
+  
+    module.exports = {
+      registerUser, 
+      getUserList, 
+      editUser, 
+      deleteUser,
+      findUser
+    }
