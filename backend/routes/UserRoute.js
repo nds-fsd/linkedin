c3ee4051a6e75869e18f7325fb5385dd0a132027
dadaisma <114525596+dadaisma@users.noreply.github.com>
2022-12-19 10:53:05 +0100

    update user_routes

--- backend/routes/UserRoute.js
+++ backend/routes/UserRoute.js
@@ -2,7 +2,7 @@ const { Router } = require("express");
 
 const UserModel = require("../mongo/schema/user/user")
 
- const {registerUser, getUserList, editUser, deleteUser } = require("../utils/UserController");
+ const {registerUser, getUserList, editUser, deleteUser, findUser } = require("../utils/UserController");
  
  const routerUser = Router();
 
@@ -11,5 +11,6 @@ routerUser.post("/user", registerUser);
   routerUser.get("/user", getUserList);
   routerUser.patch/("/user/:id", editUser);
   routerUser.delete/("/user/:id", deleteUser);
+  routerUser.get/("user/:id", findUser);
 
   module.exports = routerUser
