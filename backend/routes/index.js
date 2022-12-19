bc90ebd43a6ceb1bdfbadbd61c19a6baa33ec2c4
ubuntu <ocaride@gmail.com>
2022-12-19 19:33:58 +0100

    error order companyroute

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const router = express.Router()
+
 const userRoutes = require("./user")
 const jobRoutes = require("./job")
+const companyRoutes = require("./CompanyRoute")
 
 
 router.use("/user", userRoutes)
 router.use("/job", jobRoutes)
 router.use("/company",companyRoutes)
 
-const companyRoutes = require("./CompanyRoute")
 
 module.exports = router;
