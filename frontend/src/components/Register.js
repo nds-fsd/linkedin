5be70316f5a15f6cd496c1123ca340ce951a4f44
David Mas-Bagà <davidmasbaga@gmail.com>
2023-01-13 00:39:46 +0100

    modificaciones register front

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -1,43 +1,55 @@
 import React, {useState} from 'react';
 import  "./logina.css";
+import { apiWrapper } from "../utils/apiWrapper"
 import Logo from "./logo"
 
 const Register= (props) => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const [name, setName] = useState('');
+    const [username, setName] = useState('');
     const [surname, setSurname] =useState('')
 
-    const handleSubmit = () => {
-        const url = 'http://localhost:3001/';
-        const body = {
-            name,
-            surname,
-            email,
-            password
-        };
+    const handleName = (e)=>{
+        setName(e.target.value);
+        console.log(username)
         
-        const options = {
-            method: 'POST',
-            mode: 'cors',
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json'
-            },
-            body: JSON.stringify(body),
-        }
-        fetch(url, options)
-        .then((response) => response.json())
-        .then(data => {
-            console.log(data);
-            reloadPage();
-        });
-    }
+    };
+    const handleSurname = (e)=>{
+        setSurname(e.target.value);
+        
+    };
+    const handleEmail = (e)=>{
+        setEmail(e.target.value);
+        
+    };
+    const handlePassword = (e)=>{
+        setPassword(e.target.value);
+        
+    };
 
+    const handleSubmit = () => {
+        const payload = {
+          username: username,
+          surname: surname,
+          password: password,
+          email: email,
+          
+        }
+        
+        return fetch('http://localhost:3001/user', {
+          method: 'POST',
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(payload)
+        }).then(response => response.json());
+      }
+    
     // const handleSubmit = (e) => {
     //     e.preventDefault();
     //     console.log(email);}
-
+   
         return (
             
             <>   
@@ -54,20 +66,21 @@ const Register= (props) => {
 
             <div className="auth-form-container">
                 <h2>Rellena el formulario y únete a JobLink</h2>
-            <form className="register-form" onSubmit={handleSubmit}>
+            <form className="register-form" onSubmit={apiWrapper}>
             <div className='reg-img'>
             <img src="./img/Vector.png" alt="logo" />
             </div>
                 <label htmlFor="name">Nombre</label>
-                <input value={name} name="name" onChange={(e) => setName(e.target.value)} id="name" placeholder="Name" />
+                <input value={username} name="name" onChange={handleName} id="name" placeholder="Name" />
                 <label htmlFor="name">Apellidos</label>
-                <input value={surname} name="surname" onChange={(e) => setSurname(e.target.value)} id="surname" placeholder="Surname" />
+                <input value={surname} name="surname" onChange={handleSurname} id="surname" placeholder="Surname" />
                 
                 <label htmlFor="email">Correo electrónico</label>
-                <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="youremail@gmail.com" id="email" name="email" />
+                <input value={email} onChange={handleEmail}type="email" placeholder="youremail@gmail.com" id="email" name="email" />
                 <label htmlFor="password">Elige una contraseña</label>
-                <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="********" id="password" name="password" />
-                <button type="submit" onClick={handleSubmit}>Registrarme</button>
+                <input value={password} onChange={handlePassword} type="password" placeholder="********" id="password" name="password" />
+
+                <button type="submit" onSubmit={handleSubmit}>Registrarme</button>
                 <button className="link-btn" onClick={() => props.onFormSwitch('login')}>Already have an account? Login here.</button>
             </form>
            
