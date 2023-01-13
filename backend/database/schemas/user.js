a13b9149fab54651e641bbc8de487c7b349c71e9
David Mas-Bagà <davidmasbaga@gmail.com>
2023-01-13 18:31:22 +0100

    Registro Usuario Front y API Wrapper

--- backend/database/schemas/user.js
+++ backend/database/schemas/user.js
@@ -2,7 +2,7 @@ const { Schema, model } = require('mongoose');
 
 const userSchema = new Schema({
   username:{type: String},
-  //email: { type: String, required: true },
+  email: { type: String,},
   password:{type: String},
   //surname: { type: String, required: true },
   // nombre: { type: String, required: true },
--- frontend/src/utils/apiWrapper.js
+++ frontend/src/utils/apiWrapper.js
@@ -9,11 +9,20 @@ export const apiWrapper = (url, method, body = {},  headers = {}) => {
         },
         body: JSON.stringify(body),
     }
-    return fetch(`http:localhost:3001/${url}`, options)
+    return fetch("http://localhost:3001/"+url, options)
+    
+    
+    
         .then((response) => {
+            console.log(body)
+            console.log(response.status)
+            console.log(url)
+
             if (response.status >= 200 && response.status < 300) {
+                console.log("2")
                 return response.json();
             } else {
+                console.log("3")
                 return Promise.reject();
             }
         });
