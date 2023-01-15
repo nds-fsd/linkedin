86d68c36f74829f232ae24b1076349db74b389b1
David Mas-Bagà <davidmasbaga@gmail.com>
2023-01-15 20:36:06 +0100

    a

--- backend/database/schemas/user.js
+++ backend/database/schemas/user.js
@@ -4,7 +4,7 @@ const userSchema = new Schema({
   username:{type: String, required:true},
   email: { type: String, required: true },
   password:{type: String, required: true},
-  surname: { type: String, required: true },
+  // surname: { type: String, required: true },
   // nombre: { type: String, required: true },
   // apellido2: { type: String },
   
