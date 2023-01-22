6d5712f8d4adba7d2333792a331ed23eb236b10a
dadaisma <114525596+dadaisma@users.noreply.github.com>
2023-01-22 16:38:44 +0100

    fixed_menu

--- frontend/src/components/jobs/JobCard.js
+++ frontend/src/components/jobs/JobCard.js
@@ -1,14 +1,25 @@
+import "./JobCard.css"
+
 const JobCard = (
     {_id, companyName, jobPosition, 
-        jobDescription, 
+        jobDescription, salary,
         company_logo_url }) => {
     return (
-      <div>
-        <h2>{companyName}</h2>
-        <h3>{jobPosition}</h3>
-        <p>{jobDescription}</p>
+        <>       
+        
+      <div className="container__post">
+      {/* <div className="header__post">
+        <img src="./img/vector.png" alt="ops" />
+            <h1> New Job Offers:</h1>
+            </div> */}
+       <p> Company:</p><h2>{companyName}</h2>
+        <p>Job Position:</p><h3>{jobPosition}</h3>
+        <p>Job Description:<br></br>{jobDescription}</p>
+        <p>Salary:</p><h3>{salary} €</h3>
         <img src={company_logo_url} alt={`${companyName} logo`} />
       </div>
+      </>
+
     )
 }
 
--- frontend/src/components/jobs/Jobs_map.js
+++ frontend/src/components/jobs/Jobs_map.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import JobCard from './JobCard';
+import "./Jobs_map.css"
 
 const Jobsmap = () => {
     const [job, setJob] = useState([]);
@@ -15,11 +16,18 @@ const Jobsmap = () => {
     }, [refresh]);
 
     return (
+        <>
+         
         <div className='jobmap'>
+        <div className="header__post">
+        <img src="./img/vector.png" alt="ops" />
+            <h2> New Job Offers:</h2>
+            </div> 
             {job.map(job => (
                 <JobCard key={job._id} {...job} />
             ))}
         </div>
+        </>
     )
 }
 
