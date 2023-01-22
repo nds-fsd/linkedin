2dda3a69c4827a9350892d847129f0043795bd5c
dadaisma <114525596+dadaisma@users.noreply.github.com>
2023-01-22 17:26:26 +0100

    fix_padding_right_side_home

--- frontend/src/components/jobs/Jobs_map.js
+++ frontend/src/components/jobs/Jobs_map.js
@@ -2,6 +2,21 @@ import React, { useState, useEffect } from 'react';
 import JobCard from './JobCard';
 import "./Jobs_map.css"
 
+// const [job, setJob] = useState([]);
+// const [refresh, setRefresh] = useState(false);
+
+// const memoizedJob = useMemo(() => {
+//     async function fetchData() {
+//         const response = await fetch('http://localhost:3001/job');
+//         const data = await response.json();
+//         return data;
+//     }
+//     return fetchData();
+// }, [refresh]);
+
+// useEffect(() => {
+//     setJob(memoizedJob);
+// }, [memoizedJob]);
 const Jobsmap = () => {
     const [job, setJob] = useState([]);
     const [refresh, setRefresh] = useState(false);
@@ -22,6 +37,7 @@ const Jobsmap = () => {
         <div className="header__post">
         <img src="./img/vector.png" alt="ops" />
             <h2> New Job Offers:</h2>
+            <button onClick={() => setRefresh(true)}>Refresh</button>
             </div> 
             {job.map(job => (
                 <JobCard key={job._id} {...job} />
