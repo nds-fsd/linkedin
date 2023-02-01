bd8286ebabb02121d851709e774a58b20c88df9d
Lucian <lucianmihalca@hotmail.com>
2023-02-01 20:13:15 +0100

    linea creada

--- frontend/src/pages/error.js
+++ frontend/src/pages/error.js
@@ -1,7 +1,11 @@
+import styles from "./error.module.css";
 const ErrorPage = (props) => {
   return (
     <>
+      
+      <div className={styles.general}>
       <p>404 - Error Page ...</p>
+      </div>
      
     </>
   );
