64e877fa0de2c38567334e8353a90f64dfde171c
Oscar Caride Moreno <ocaride@gmail.com>
2023-02-02 22:16:32 +0100

    se aplica el apiWrapper al CRUD del adminPage

--- frontend/src/components/modal/element/modalElementCompany.js
+++ frontend/src/components/modal/element/modalElementCompany.js
@@ -1,12 +1,102 @@
 import React, { useEffect, useState } from "react";
 import { camelCase } from "../../../utils/functions";
+import ModalElement from "./modalElement";
+import { apiWrapper } from "../../../utils/apiWrapper";
 import styles from "./modalElement.module.css";
+import { toast } from "react-toastify";
 
 const ModalElementCompany = (props) => {
   const [title, setTitle] = useState("");
-  const handleClick = () => {
-    props.onClose();
+  const [openModalConfirm, setOpenModalConfirm] = useState(false);
+  const [modalconfirmation, setModalConfirmation] = useState(false);
+
+  const [nombre, setNombre] = useState("");
+  const [web, setWeb] = useState("");
+  const [sector, setSector] = useState("");
+  const [nTrabajadores, setNTrabajadores] = useState(0);
+
+  const saveModalConfirmation = (param) => {
+    setModalConfirmation(param);
+
+    if (param === true) {
+      switch (props.statusClick) {
+        case "C": //CREATE
+          console.log("Confirm OK CREATE");
+          sendBackendPOSTCreate();
+          break;
+        case "U": //UPDATE
+          console.log("Confirm OK UPDATE");
+          sendBackendUPDATE();
+          toast("Confirm OK UPDATE");
+          break;
+        case "D": //DELETE
+          console.log("Confirm OK DELETE");
+          sendBackendDELETE();
+          toast("Confirm OK DELETE");
+          break;
+      }
+      props.onClose();
+    } else {
+      if (props.statusClick === "D") {
+        props.onClose();
+      }
+    }
   };
+
+  const sendBackendPOSTCreate = async () => {
+    apiWrapper("company", "POST", {
+      name: nombre,
+      website: web,
+      company_type: sector,
+      company_size: nTrabajadores,
+    }).then((res) => {
+      console.log(res);
+    });
+
+    //TODO : No funciona
+    /*
+    toast.success("Usuario Creado correctamente", {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+    */
+  };
+
+  const sendBackendUPDATE = () => {
+    apiWrapper(`company/${props.itemId.replace('"', "").replace('"', "")}`, "PATCH", {
+      name: nombre,
+      website: web,
+      company_type: sector,
+      company_size: nTrabajadores,
+    }).then((res) => {
+      console.log(res);
+    });
+  };
+
+  const sendBackendDELETE = () => {
+    apiWrapper(`company/${props.itemId.replace('"', "").replace('"', "")}`, "DELETE").then(
+      (payload) => {
+        console.log(payload);
+      }
+    );
+  };
+
+  const handleClick = (param) => {
+    //si pulsamos el boton de OK (Guardar), forzamos un segundo modal de Confirmacion
+    if (
+      param === "sendConfirm" &&
+      (props.statusClick === "C" || props.statusClick === "U") &&
+      openModalConfirm === false
+    ) {
+      setOpenModalConfirm(true); //llamamos al Modal->Confirm
+    } else props.onClose();
+  };
+
   useEffect(() => {
     //console.log(`${camelCase(props.titulo)} ( ${props.itemId} ) ${props.statusClick}`);
 
@@ -14,19 +104,49 @@ const ModalElementCompany = (props) => {
     switch (props.statusClick) {
       case "C":
         setTitle(`Crear un nuevo Registro de "${camelCase(props.titulo)}"`);
-        break;
-      case "U":
-        setTitle(`Actualizar un nuevo Registro de "${camelCase(props.titulo)}"`);
+        setNombre("");
+        setWeb("");
+        setSector("");
+        setNTrabajadores(0);
+
         break;
       case "R":
         setTitle(`Consultar un nuevo Registro de "${camelCase(props.titulo)}"`);
+        //Leemos los datos de la ficha del usuario
+        apiWrapper(`company/${props.itemId.replace('"', "").replace('"', "")}`, "GET").then(
+          (res) => {
+            setNombre(res.name);
+            setWeb(res.website);
+            setSector(res.company_type);
+            setNTrabajadores(res.company_size);
+          }
+        );
+        break;
+      case "U":
+        setTitle(`Actualizar un nuevo Registro de "${camelCase(props.titulo)}"`);
+        //Leemos los datos de la ficha del usuario
+        apiWrapper(`company/${props.itemId.replace('"', "").replace('"', "")}`, "GET").then(
+          (res) => {
+            setNombre(res.name);
+            setWeb(res.website);
+            setSector(res.company_type);
+            setNTrabajadores(res.company_size);
+          }
+        );
         break;
       case "D":
         setTitle(`Eliminar un nuevo Registro de "${camelCase(props.titulo)}"`);
         break;
       default:
     }
+    
+    if (props.statusClick === "D" && openModalConfirm === false) {
+      console.log("DELETE");
+      //llamamos al Modal->Confirm
+      setOpenModalConfirm(true);
+    }
   }, [props.statusClick]);
+
   return (
     <div className={styles.fondo}>
       <h2>{title} </h2>
@@ -36,32 +156,48 @@ const ModalElementCompany = (props) => {
           type="text"
           required="required"
           disabled={props.statusClick === "R" ? "disabled" : null}
+          value={nombre}
+          onChange={(e) => {
+            setNombre(e.target.value);
+          }}
         />
-        <span>Nombre</span>
+        <span hidden={props.statusClick === "R" && nombre!=="" ? "hidden" : null}>Nombre</span>
       </div>
       <div className={styles.inputBox}>
         <input
           type="text"
           required="required"
           disabled={props.statusClick === "R" ? "disabled" : null}
+          value={web}
+          onChange={(e) => {
+            setWeb(e.target.value);
+          }}
         />
-        <span>Web</span>
+        <span hidden={props.statusClick === "R" && web!=="" ? "hidden" : null}>Web</span>
       </div>
       <div className={styles.inputBox}>
         <input
           type="text"
           required="required"
           disabled={props.statusClick === "R" ? "disabled" : null}
+          value={sector}
+          onChange={(e) => {
+            setSector(e.target.value);
+          }}
         />
-        <span>Sector</span>
+        <span hidden={props.statusClick === "R" && sector!=="" ? "hidden" : null}>Sector</span>
       </div>
       <div className={styles.inputBox}>
         <input
           type="text"
           required="required"
           disabled={props.statusClick === "R" ? "disabled" : null}
+          value={nTrabajadores}
+          onChange={(e) => {
+            setNTrabajadores(e.target.value);
+          }}
         />
-        <span>Nº Trabajadores</span>
+        <span hidden={props.statusClick === "R" && nombre!=="" ? "hidden" : null}>Nº Trabajadores</span>
       </div>
       {props.statusClick === "R" && (
         <div className={styles.botones}>
@@ -74,7 +210,7 @@ const ModalElementCompany = (props) => {
       {(props.statusClick === "C" || props.statusClick === "U") && (
         <div className={styles.botones}>
           <div>
-            <button className={styles.botonOK} onClick={handleClick}>
+            <button className={styles.botonOK} onClick={() => handleClick("sendConfirm")}>
               Guardar
             </button>{" "}
             &nbsp;
@@ -86,6 +222,21 @@ const ModalElementCompany = (props) => {
           </div>
         </div>
       )}
+      <ModalElement
+        open={openModalConfirm}
+        onClose={() => setOpenModalConfirm(false)}
+        element="confirm"
+        mensaje={`¿Esta seguro de ${
+          props.statusClick === "C"
+            ? "crear"
+            : props.statusClick === "U"
+            ? "actualizar"
+            : props.statusClick === "D"
+            ? "eliminar"
+            : ""
+        } el registro?`}
+        saveModalConfirmation={saveModalConfirmation}
+      />
     </div>
   );
 };
--- frontend/src/components/modal/element/modalElementPost.js
+++ frontend/src/components/modal/element/modalElementPost.js
@@ -1,12 +1,100 @@
 import React, { useEffect, useState } from "react";
 import { camelCase } from "../../../utils/functions";
+import ModalElement from "./modalElement";
+import { apiWrapper } from "../../../utils/apiWrapper";
 import styles from "./modalElement.module.css";
+import { toast } from "react-toastify";
 
 const ModalElementPost = (props) => {
   const [title, setTitle] = useState("");
-  const handleClick = () => {
-    props.onClose();
+  const [openModalConfirm, setOpenModalConfirm] = useState(false);
+  const [modalconfirmation, setModalConfirmation] = useState(false);
+
+  const [titulo, setTitulo] = useState("");
+  const [descripcion, setDescripcion] = useState("");
+  const [contenido, setContenido] = useState("");
+  const [comentarios, setComentarios] = useState("");
+
+  const saveModalConfirmation = (param) => {
+    setModalConfirmation(param);
+
+    if (param === true) {
+      switch (props.statusClick) {
+        case "C": //CREATE
+          console.log("Confirm OK CREATE");
+          sendBackendPOSTCreate();
+          break;
+        case "U": //UPDATE
+          console.log("Confirm OK UPDATE");
+          sendBackendUPDATE();
+          toast("Confirm OK UPDATE");
+          break;
+        case "D": //DELETE
+          console.log("Confirm OK DELETE");
+          sendBackendDELETE();
+          toast("Confirm OK DELETE");
+          break;
+      }
+      props.onClose();
+    } else {
+      if (props.statusClick === "D") {
+        props.onClose();
+      }
+    }
   };
+
+  const sendBackendPOSTCreate = async () => {
+    apiWrapper("post", "POST", {
+      title: titulo,
+      description: descripcion,
+      content: contenido,
+      comments: comentarios,
+    }).then((res) => {
+      console.log(res);
+    });
+
+    //TODO : No funciona
+    toast.success("Usuario Creado correctamente", {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+  };
+
+  const sendBackendUPDATE = () => {
+    apiWrapper(`post/${props.itemId.replace('"', "").replace('"', "")}`, "PATCH", {
+      title: titulo,
+      description: descripcion,
+      content: contenido,
+      comments: comentarios,
+    }).then((res) => {
+      console.log(res);
+    });
+  };
+
+  const sendBackendDELETE = () => {
+    apiWrapper(`post/${props.itemId.replace('"', "").replace('"', "")}`, "DELETE").then(
+      (payload) => {
+        console.log(payload);
+      }
+    );
+  };
+
+  const handleClick = (param) => {
+    //si pulsamos el boton de OK (Guardar), forzamos un segundo modal de Confirmacion
+    if (
+      param === "sendConfirm" &&
+      (props.statusClick === "C" || props.statusClick === "U") &&
+      openModalConfirm === false
+    ) {
+      setOpenModalConfirm(true); //llamamos al Modal->Confirm
+    } else props.onClose();
+  };
+
   useEffect(() => {
     //console.log(`${camelCase(props.titulo)} ( ${props.itemId} ) ${props.statusClick}`);
 
@@ -14,17 +102,41 @@ const ModalElementPost = (props) => {
     switch (props.statusClick) {
       case "C":
         setTitle(`Crear un nuevo Registro de "${camelCase(props.titulo)}"`);
-        break;
-      case "U":
-        setTitle(`Actualizar un nuevo Registro de "${camelCase(props.titulo)}"`);
+        setTitulo("");
+        setDescripcion("");
+        setContenido("");
+        setComentarios("");
         break;
       case "R":
         setTitle(`Consultar un nuevo Registro de "${camelCase(props.titulo)}"`);
+        //Leemos los datos de la ficha del usuario
+        apiWrapper(`post/${props.itemId.replace('"', "").replace('"', "")}`, "GET").then((res) => {
+          setTitulo(res.title);
+          setDescripcion(res.description);
+          setContenido(res.content);
+          setComentarios(res.comments);
+        });
+        break;
+      case "U":
+        setTitle(`Actualizar un nuevo Registro de "${camelCase(props.titulo)}"`);
+        //Leemos los datos de la ficha del usuario
+        apiWrapper(`post/${props.itemId.replace('"', "").replace('"', "")}`, "GET").then((res) => {
+          setTitulo(res.title);
+          setDescripcion(res.description);
+          setContenido(res.content);
+          setComentarios(res.comments);
+        });
         break;
       case "D":
         setTitle(`Eliminar un nuevo Registro de "${camelCase(props.titulo)}"`);
         break;
       default:
+    }
+    
+    if (props.statusClick === "D" && openModalConfirm === false) {
+      console.log("DELETE");
+      //llamamos al Modal->Confirm
+      setOpenModalConfirm(true);
     }
   }, [props.statusClick]);
 
@@ -37,32 +149,48 @@ const ModalElementPost = (props) => {
           type="text"
           required="required"
           disabled={props.statusClick === "R" ? "disabled" : null}
+          value={titulo}
+          onChange={(e) => {
+            setTitulo(e.target.value);
+          }}
         />
-        <span>Titulo</span>
+        <span hidden={props.statusClick === "R" && titulo!=="" ? "hidden" : null}>Titulo</span>
       </div>
       <div className={styles.inputBox}>
         <input
           type="text"
           required="required"
           disabled={props.statusClick === "R" ? "disabled" : null}
+          value={descripcion}
+          onChange={(e) => {
+            setDescripcion(e.target.value);
+          }}
         />
-        <span>Descripcion</span>
+        <span hidden={props.statusClick === "R" && descripcion!=="" ? "hidden" : null}>Descripcion</span>
       </div>
       <div className={styles.inputBox}>
         <input
           type="text"
           required="required"
           disabled={props.statusClick === "R" ? "disabled" : null}
+          value={contenido}
+          onChange={(e) => {
+            setContenido(e.target.value);
+          }}
         />
-        <span>Contenido</span>
+        <span hidden={props.statusClick === "R" && contenido!=="" ? "hidden" : null}>Contenido</span>
       </div>
       <div className={styles.inputBox}>
         <input
           type="text"
           required="required"
           disabled={props.statusClick === "R" ? "disabled" : null}
+          value={comentarios}
+          onChange={(e) => {
+            setComentarios(e.target.value);
+          }}
         />
-        <span>Comentarios</span>
+        <span hidden={props.statusClick === "R" && comentarios!=="" ? "hidden" : null}>Comentarios</span>
       </div>
       {props.statusClick === "R" && (
         <div className={styles.botones}>
@@ -75,7 +203,7 @@ const ModalElementPost = (props) => {
       {(props.statusClick === "C" || props.statusClick === "U") && (
         <div className={styles.botones}>
           <div>
-            <button className={styles.botonOK} onClick={handleClick}>
+            <button className={styles.botonOK} onClick={() => handleClick("sendConfirm")}>
               Guardar
             </button>{" "}
             &nbsp;
@@ -87,7 +215,23 @@ const ModalElementPost = (props) => {
           </div>
         </div>
       )}
+        <ModalElement
+        open={openModalConfirm}
+        onClose={() => setOpenModalConfirm(false)}
+        element="confirm"
+        mensaje={`¿Esta seguro de ${
+          props.statusClick === "C"
+            ? "crear"
+            : props.statusClick === "U"
+            ? "actualizar"
+            : props.statusClick === "D"
+            ? "eliminar"
+            : ""
+        } el registro?`}
+        saveModalConfirmation={saveModalConfirmation}
+      />
     </div>
+    
   );
 };
 
