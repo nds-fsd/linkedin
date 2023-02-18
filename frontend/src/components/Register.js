a68a3bf6fa35ca8dea5eb90bdf57c3d45f3974f6
dadaisma <114525596+dadaisma@users.noreply.github.com>
2023-02-18 23:04:02 +0100

    Register_ReactHookForms_workin_but_notoken

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -1,24 +1,55 @@
-import React, { useState } from 'react';
+import React from "react";
+import { useForm } from "react-hook-form";
 import "./logina.css";
-import { apiWrapper } from "../utils/apiWrapper"
-import Logo from "./logo"
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { apiWrapper } from "../utils/apiWrapper";
+import Logo from "./logo";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import { useNavigate } from "react-router-dom";
-import {Submit_register} from './Login';
-
+import { Submit_register } from "./Login";
 
 const Register = (props) => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [nombre, setNombre] = useState('');
-    const [apellido, setApellido] = useState('');
-    const navigate = useNavigate();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const navigate = useNavigate();
+
+  const handleSuccessfulRegistration = (data) => {
+    console.log(data)
+    Submit_register({ ...data, nombre:data.nombre, password: data.password }, useNavigate);
+    toast.success("Registrado Correctamente! Vamos a la Home page...", {
+      position: "top-center",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
    
-    const handleSuccessfulRegistration = (data) => {
-      console.log(data)
-      Submit_register({ ...data, nombre:nombre, password: password }, useNavigate);
-      toast.success("Registrado Correctamente! Vamos a la Home page...", {
+    setTimeout(() => {
+      navigate("/home");
+    }, 5000);
+  };
+
+  const onSubmit = async (data) => {
+    try {
+        await apiWrapper("user/register", "POST", {
+          nombre:data.nombre,
+          email:data.email,
+            password:data.password,
+            apellido:data.apellido
+        })
+        .then(
+            (data)=> {
+console.log(data);
+handleSuccessfulRegistration(data);
+      });
+    } catch (error) {
+      console.error(error);
+      toast.error("An error occurred. Please try again.", {
         position: "top-center",
         autoClose: 5000,
         hideProgressBar: false,
@@ -27,134 +58,92 @@ const Register = (props) => {
         draggable: true,
         progress: undefined,
       });
-     
-      setTimeout(() => {
-        navigate("/home");
-      }, 5000);
-    };
-   
-
-    const handleNombre = (e) => {
-        setNombre(e.target.value);
-    };
-
-    const handleApellido = (e) => {
-        setApellido(e.target.value);
-    };
-
-    const handleEmail = (e) => {
-        setEmail(e.target.value);
-    };
-    const handlePassword = (e) => {
-        setPassword(e.target.value);
-    };
-
-    return (
-
-        <>
-             <nav className="navbar" onClick={() => navigate("/")}>
-          <div className="navbar_logo">
-            <Logo />
-            <h2>JobLink</h2>
-          </div>
-          <div className="navbar_button">
-            <button className="navbtn">Login</button>
-          </div>
-        </nav>
-
-            <div className="auth-form-container">
-            <ToastContainer 
-            position="top-center" 
-            autoClose={5000} 
-            hideProgressBar={false} 
-            newestOnTop={false} 
-            closeOnClick rtl={false} 
-            pauseOnFocusLoss 
-            draggable 
-            pauseOnHover />
-
-                <h2>Rellena el formulario y únete a JobLink</h2>
-                <div className="register-form" >
-                    <div className='reg-img'>
-                        <img src="./img/Vector.png" alt="logo" />
-                    </div>
-                    <label htmlFor="nombre">Nombre</label>
-
-                    <input
-                        value={nombre}
-                        name="name"
-                        onChange={handleNombre}
-                        id="username"
-                        placeholder="Nombre"
-                    />
-                    <label htmlFor="apellido">Apellido</label>
-
-                    <input
-                        value={apellido}
-                        name="name"
-                        onChange={handleApellido}
-                        id="username"
-                        placeholder="Nombre"
-                    />
-
-                    <label htmlFor="email">Correo electrónico</label>
-
-                    <input
-                        value={email}
-                        onChange={handleEmail}
-                        type="email"
-                        placeholder="youremail@gmail.com"
-                        id="email"
-                        name="email"
-                    />
-
-                    <label htmlFor="password">Elige una contraseña</label>
-                    <input value={password}
-                        onChange={handlePassword}
-                        type="password"
-                        placeholder="********"
-                        id="password"
-                        name="password"
-                    />
-
-<button className="submit-button" type="submit" 
-                onClick={async () => {
-                    try {
-                        await apiWrapper("user/register", "POST", {
-                            nombre:nombre,
-                            email:email,
-                            password:password,
-                            apellido:apellido
-                        })
-                        .then(
-                            (data)=> {
-console.log(data);
-handleSuccessfulRegistration(data);
-     });
-
-    } catch (error) {
-        console.error(error);
-        toast.error("An error occurred. Please try again.", {
-            position: "top-center",
-            autoClose: 5000,
-            hideProgressBar: false,
-            closeOnClick: true,
-            pauseOnHover: true,
-            draggable: true,
-            progress: undefined,
-        });
     }
-}}>Registrarme</button><br></br>
-                    <button
-                        className="link-btn"
-                        onClick={() => navigate("/")}
-                    >Already have an account? Login here.
-                    </button>
+  };
+  return (
+    <>
+      <nav className="navbar" onClick={() => navigate("/")}>
+        <div className="navbar_logo">
+          <Logo />
+          <h2>JobLink</h2>
+        </div>
+        <div className="navbar_button">
+          <button className="navbtn">Login</button>
+        </div>
+      </nav>
+      <div className="auth-form-container">
+        <ToastContainer
+          position="top-center"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+        />
 
-                </div>
+        <h2>Rellena el formulario y únete a JobLink</h2>
+        <form className="register-form" onSubmit={handleSubmit(onSubmit)}>
+          <div className="reg-img">
+            <img src="./img/Vector.png" alt="logo" />
+          </div>
 
-            </div>
-        </>
-    )
-}
-export default Register
+          <label htmlFor="nombre">Nombre</label>
+          <input
+            type="text"
+            placeholder="Nombre"
+            {...register("nombre", { required: true })}
+          />
+          {errors.nombre && (
+            <span className="error-msg">Este campo es requerido</span>
+          )}
+
+          <label htmlFor="apellido">Apellido</label>
+          <input
+            type="text"
+            placeholder="Apellido"
+            {...register("apellido", { required: true })}
+          />
+          {errors.apellido && (
+            <span className="error-msg">Este campo es requerido</span>
+          )}
+
+<label htmlFor="email">Correo electrónico</label>
+<input
+  type="email"
+  placeholder="youremail@gmail.com"
+  {...register("email", { required: true })}
+/>
+{errors.email && (
+  <span className="error-msg">Este campo es requerido</span>
+)}
+
+<label htmlFor="password">Elige una contraseña</label>
+<input
+  type="password"
+  placeholder="********"
+  id="password"
+  name="password"
+  {...register("password", { required: true })}
+/>
+{errors.password && (
+  <span className="error-msg">Este campo es requerido</span>
+)}
+
+<button className="submit-button" type="submit">
+  Registrarme
+</button>
+
+<button className="link-btn" onClick={() => navigate("/")}>
+  ¿Ya tienes una cuenta? Inicia sesión aquí.
+</button>
+</form>
+</div>
+</>
+);
+};
+
+export default Register;
+
