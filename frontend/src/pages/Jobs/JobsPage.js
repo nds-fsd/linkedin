005a53b50001890f81a6ca0dc99a56eacc34c7e8
Oscar Caride Moreno <ocaride@gmail.com>
2023-03-05 18:01:26 +0100

    Si eres owner de una empresa, aparecera la opcion de Generador de Ofertas

--- frontend/src/pages/Jobs/JobsPage.js
+++ frontend/src/pages/Jobs/JobsPage.js
@@ -6,18 +6,27 @@ import ListJobs from "../../components/jobs/list/listJobs";
 import LinkJobs from "../../components/jobs/links/linkJobs";
 import GeneratorJobs from "../../components/jobs/generator/generatorJobs";
 import ViewListIcon from "@mui/icons-material/ViewList";
-import AddLinkIcon from '@mui/icons-material/AddLink';
-import WorkIcon from '@mui/icons-material/Work';
+import AddLinkIcon from "@mui/icons-material/AddLink";
+import WorkIcon from "@mui/icons-material/Work";
+import { apiWrapper } from "../../utils/apiWrapper";
 
 const JobsPage = (props) => {
   const { idUser } = useParams();
+  const [companyOwner, setCompanyOwner] = useState({});
 
   const [selectOptions, setSelectOptions] = useState(1);
   const handleClickSelectOptions = (value) => {
     setSelectOptions(value);
   };
 
-  useEffect(() => {}, [selectOptions]);
+  /*useEffect(() => {}, [selectOptions]);*/
+
+  useEffect(() => {
+    apiWrapper("company/", "GET").then((res) => {
+      const resAux = res.filter((element) => element.owner === idUser);
+      setCompanyOwner(resAux);
+    });
+  }, []);
 
   return (
     <>
@@ -44,21 +53,26 @@ const JobsPage = (props) => {
                 <AddLinkIcon className={styles.iconos}></AddLinkIcon>
                 <h2>Ofertas Linkadas</h2>
               </div>
-              <div
-                className={selectOptions === 3 ? styles.opcionSelected : styles.opciones}
-                onClick={() => {
-                  handleClickSelectOptions(3);
-                }}
-              >
-                <WorkIcon className={styles.iconos}></WorkIcon>
-                <h2>Generador de Ofertas</h2>
-              </div>
+
+              {companyOwner.length > 0 && (
+                <div
+                  className={selectOptions === 3 ? styles.opcionSelected : styles.opciones}
+                  onClick={() => {
+                    handleClickSelectOptions(3);
+                  }}
+                >
+                  <WorkIcon className={styles.iconos}></WorkIcon>
+                  <h2>Generador de Ofertas</h2>
+                </div>
+              )}
             </div>
             <div className={styles.derecha}>
               <div className={styles.contenedor}>
-                {selectOptions === 1 && <ListJobs userId={idUser}/>}
+                {selectOptions === 1 && <ListJobs userId={idUser} />}
                 {selectOptions === 2 && <LinkJobs userId={idUser} />}
-                {selectOptions === 3 && <GeneratorJobs userId={idUser} />}
+                {companyOwner.length > 0 && selectOptions === 3 && (
+                  <GeneratorJobs userId={idUser} />
+                )}
               </div>
             </div>
           </div>
